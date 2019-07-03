f515e56383021fdcf592cb44689de23e9d51d10f
Matthew Balch <matbalch@vt.edu>
2019-07-03 16:55:49 -0400

    Added case to portable_endian to enable compilation to WASM

--- src/portable_endian.h
+++ src/portable_endian.h
@@ -114,6 +114,23 @@
 #	define __LITTLE_ENDIAN LITTLE_ENDIAN
 #	define __PDP_ENDIAN    PDP_ENDIAN
 
+#elif	defined(__EMSCRIPTEN__)
+
+#		define htobe16(x) htons(x)
+#		define htole16(x) (x)
+#		define be16toh(x) ntohs(x)
+#		define le16toh(x) (x)
+ 
+#		define htobe32(x) htonl(x)
+#		define htole32(x) (x)
+#		define be32toh(x) ntohl(x)
+#		define le32toh(x) (x)
+ 
+#		define htobe64(x) htonll(x)
+#		define htole64(x) (x)
+#		define be64toh(x) ntohll(x)
+#		define le64toh(x) (x)
+
 #else
 
 #	error platform not supported
